cbd6facbc791e2b5e79e77b828460f01aa0cb1fb
Özer S <o.sahin@oesah.de>
2017-10-20 18:13:13 +0200

    first tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -11,15 +11,20 @@ Tests for `djangocms-slick-slider` models module.
 from django.test import TestCase
 
 from djangocms_slick_slider import models
+from djangocms_slick_slider import settings
 
 
 class TestDjangocms_slick_slider(TestCase):
 
     def setUp(self):
-        pass
+        self.slider = models.SlickSlider()
+        self.slider.title = "Test Slider"
 
-    def test_something(self):
-        pass
+    def test_string_repr(self):
+        self.assertEqual(str(self.slider), "Test Slider")
+
+    def test_settings_default(self):
+        self.assertEqual(self.slider.settings, settings.SLIDER_DEFAULT)
 
     def tearDown(self):
         pass
