6d111468014d31eecafb136b6d537f2a9c546ba7
oesah <o.sahin@oesah.de>
2021-02-13 18:00:56 +0100

    fix: get_version according to new versioning

--- setup.py
+++ setup.py
@@ -12,16 +12,9 @@ except ImportError:
 
 def get_version(*file_paths):
     """Retrieves the version from djangocms_slick_slider/__init__.py"""
-    filename = os.path.join(os.path.dirname(__file__), *file_paths)
-    version_file = open(filename).read()
-    version_match = re.search(
-        r"^__version__ = ['\"]([^'\"]*)['\"]",
-        version_file,
-        re.M,
-    )
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
+    import djangocms_slick_slider
+
+    return djangocms_slick_slider.__version__
 
 
 version = get_version("djangocms_slick_slider", "__init__.py")
