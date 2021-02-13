7249510a75ba29f34dea435939b96eb40b8ce210
oesah <o.sahin@oesah.de>
2021-02-13 17:39:02 +0100

    feat: switch versioning to json

--- djangocms_slick_slider/__init__.py
+++ djangocms_slick_slider/__init__.py
@@ -1,1 +1,18 @@
-__version__ = '0.9.3'
+import json
+import os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+package_json = os.path.join(__location__, 'version.json')
+
+with open(package_json) as json_file:
+    data = json.load(json_file)
+
+    __version__ = data.get('version')
+    __version_info__ = tuple([
+        int(num) if num.isdigit() else num
+        for num in __version__.replace('-', '.', 1).split('.')
+    ])
+
+    json_file.close()
