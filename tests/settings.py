468ff838561579ffcf348266b6e13730d2b7c9a9
oesah <o.sahin@oesah.de>
2023-07-11 19:24:14 +0200

    chore: cleanup settings

--- tests/settings.py
+++ tests/settings.py
@@ -86,7 +86,6 @@ TEMPLATES = [
             "loaders": [
                 "django.template.loaders.filesystem.Loader",
                 "django.template.loaders.app_directories.Loader",
-                "django.template.loaders.eggs.Loader",
             ],
         },
     },
